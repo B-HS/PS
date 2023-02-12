10286c212232b5b64724ea652d5f06519d36459f
Hyunseok Byun <49316060+B-HS@users.noreply.github.com>
2023-02-12 17:02:31 +0900

    [Silver II] Title: 좌표 압축, Time: 2024 ms, Memory: 154796 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -0,0 +1,7 @@
+import sys
+sys.stdin.readline()
+target= list(map(int, sys.stdin.readline().split()))
+index_dic = sorted(list(set(target))) # 1000 999, 2 4 -10 - 9
+dic = {index_dic[i]:i for i in range(len(index_dic))}
+for i in target:
+    print(dic[i], end=" ")
