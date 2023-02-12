e5d9f16d97955d8af9f072fe431e49ae89c22a54
Hyunseok Byun <49316060+B-HS@users.noreply.github.com>
2023-02-12 15:24:59 +0900

    [Silver V] Title: 색종이, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -0,0 +1,10 @@
+ary =[[0]*100 for _ in range(100)] 
+for total in range(int(input())):
+    x, y = map(int, input().split())
+    for width in range(x, x+10):
+        for height in range(y, y+10):
+            ary[width][height]=1
+result = 0
+for i in range(100):
+    result+=sum(ary[i])
+print(result)
