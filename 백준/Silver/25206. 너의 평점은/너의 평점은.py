ba84143347aab1923feb3fab7ed0b44982da5a04
Hyunseok Byun <49316060+B-HS@users.noreply.github.com>
2023-02-28 18:18:12 +0900

    [Silver V] Title: 너의 평점은, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -0,0 +1,13 @@
+results = ['A+', 'A0', 'B+', 'B0', 'C+', 'C0', 'D+', 'D0', 'F']
+point = [4.5, 4.0, 3.5, 3.0, 2.5, 2.0, 1.5, 1.0, 0]
+
+total = 0
+result = 0
+for _ in range(20) :
+    s, p, g = input().split()
+    p = float(p)
+    if g != 'P' :
+        total += p
+        result += p * point[results.index(g)]
+
+print('%.6f' % (result / total))
