5859817485edc836be0171de3f43be66fbd3be1b
Hyunseok Byun <49316060+B-HS@users.noreply.github.com>
2023-06-27 22:29:10 +0900

    [Silver IV] Title: 암기왕, Time: 5620 ms, Memory: 186872 KB -BaekjoonHub

---
+++ 백준/Silver/2776. 암기왕/암기왕.py
@@ -0,0 +1,29 @@
+def binary_search(list, target):
+    left = 0
+    right = len(list) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if list[mid] == target:
+            return 1
+        elif list[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return 0
+
+import sys
+howmany = int(input())
+
+for _ in range(howmany):
+    input()
+    target = sorted(list(map(int, sys.stdin.readline().split())))
+    input()
+    is_target = list(map(int, sys.stdin.readline().split()))
+    
+    for i in is_target:
+        print(binary_search(target, i))
+
+
