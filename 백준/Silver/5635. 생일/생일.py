78a55fd3de416a20d11001857b1b9e98eaed9cc6
Hyunseok Byun <49316060+B-HS@users.noreply.github.com>
2023-07-01 22:07:50 +0900

    Update 생일.py

--- 백준/Silver/5635. 생일/생일.py
+++ 백준/Silver/5635. 생일/생일.py
@@ -2,3 +2,7 @@ ary = []
 for i in range(int(input())):ary.append(input().split())
 ary.sort(key=lambda x: (int(x[3]), int(x[2]), int(x[1])))
 print(ary[-1][0],ary[0][0],sep='\n')
+
+# 아래코드는 왜 index오류인지 모르겠음
+# ary=sorted([i.split() for i in open(0).read().split("\n")[1:]],key=lambda x:(int(x[3]),int(x[2]),int(x[1])))
+# print(ary[-1][0],ary[0][0],sep='\n')
