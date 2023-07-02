31190f2c29c523e8887ab77b77ec4a9753f5a044
Hyunseok Byun <49316060+B-HS@users.noreply.github.com>
2023-07-02 22:53:49 +0900

    Update 베스트셀러.py

--- 백준/Silver/1302. 베스트셀러/베스트셀러.py
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.py
@@ -1,3 +1,7 @@
 from collections import Counter;
 target = Counter([input() for _ in range(int(input()))]).most_common()
 print(sorted(target,key=lambda x:(-int(x[1]),x[0]))[0][0])
+
+# 백준에서 open(0) 코드가 잘 안먹나봄;; 
+# from collections import Counter;
+# print(sorted(Counter(open(0).read().split("\n")[1:]).most_common(),key=lambda x:(-int(x[1]),x[0]))[0][0])
